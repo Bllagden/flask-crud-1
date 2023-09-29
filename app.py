892ff3805bd4cad11fac66c430a172f88ff02319
Bllagden <bllagden99@gmail.com>
2023-09-29 19:25:24 +0400

    Implementation: Update, Delete

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # /// отноc. путь
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"  # /// отноc. путь
 db = SQLAlchemy(app)
 
 
@@ -16,23 +16,54 @@ class Todo(db.Model):
         return f"<Task {self.id}>"
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=["POST", "GET"])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+    if request.method == "POST":
+        task_content = request.form["content"]
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+        if task_content:  # Проверка на пустое поле
+            new_task = Todo(content=task_content)
+            try:
+                db.session.add(new_task)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return "Create eroor"
+        else:
+            return "Task content cannot be empty"
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template("index.html", tasks=tasks)
 
 
-if __name__ == '__main__':
+@app.route("/delete/<int:id>")
+def delete(id):
+    task = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "Delete error"
+
+
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form["content"]
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Update error"
+
+    else:
+        return render_template("update.html", task=task)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
